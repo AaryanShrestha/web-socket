624cb368d5d8077ab36ff199154f7030f5e776e0
Hemanshuxg <shresthahimanshu888@gmail.com>
2024-12-04 21:55:39 +0545

    web socket basic

--- controller/dataController.js
+++ controller/dataController.js
@@ -3,44 +3,67 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 exports.getData = catchAsyncErrors(async (req, res, next) => {
   const data = await Data.find();
+
+  // Broadcast all data to WebSocket clients
+  req.app.locals.broadcast({
+    type: "all_data",
+    data: data,
+  });
+
   res.status(200).json({ success: true, data });
 });
 
 exports.postData = catchAsyncErrors(async (req, res, next) => {
-  const newData = await Data.create(req.body);
+  // Create new data
+  await Data.create(req.body);
 
-  // Broadcast new data to WebSocket clients
+  // Fetch all data from the model
+  const allData = await Data.find();
+
+  // Broadcast all data to WebSocket clients
   req.app.locals.broadcast({
-    type: "new_data",
-    payload: newData,
+    type: "all_data",
+    data: allData,
   });
 
-  res.status(201).json({ success: true, data: newData });
+  res.status(201).json({ success: true, message: "Data added successfully!" });
 });
 
 exports.updateData = catchAsyncErrors(async (req, res, next) => {
-  const updatedData = await Data.findByIdAndUpdate(req.body.id, req.body, {
+  // Update data in the model
+  await Data.findByIdAndUpdate(req.body.id, req.body, {
     new: true,
     runValidators: true,
   });
 
-  // Broadcast updated data to WebSocket clients
+  // Fetch all data from the model
+  const allData = await Data.find();
+
+  // Broadcast all data to WebSocket clients
   req.app.locals.broadcast({
-    type: "update_data",
-    payload: updatedData,
+    type: "all_data",
+    data: allData,
   });
 
-  res.status(200).json({ success: true, data: updatedData });
+  res
+    .status(200)
+    .json({ success: true, message: "Data updated successfully!" });
 });
 
 exports.deleteData = catchAsyncErrors(async (req, res, next) => {
-  const deletedData = await Data.findByIdAndDelete(req.body.id);
+  // Delete data from the model
+  await Data.findByIdAndDelete(req.body.id);
 
-  // Broadcast deletion event to WebSocket clients
+  // Fetch all data from the model
+  const allData = await Data.find();
+
+  // Broadcast all data to WebSocket clients
   req.app.locals.broadcast({
-    type: "delete_data",
-    payload: { id: req.body.id },
+    type: "all_data",
+    data: allData,
   });
 
-  res.status(200).json({ success: true, message: "Data deleted successfully" });
+  res
+    .status(200)
+    .json({ success: true, message: "Data deleted successfully!" });
 });
